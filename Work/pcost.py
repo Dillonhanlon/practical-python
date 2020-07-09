1579491aa1d553648086a87fc14f7e19f6055a27
DillonHanlon <dfh031@mun.ca>
2020-07-08 22:47:34 -0230

    Completed exercise 1.33, turned pcost.py into function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
-tot_cost = 0
 
-f = open('Data/portfolio.csv','rt')
-headers = next(f)
-for line in f:
-    row = line.split(',')
-    shares = int(row[1])
-    share_price = float(row[2])
-    tot_cost += share_price * shares
-print(f'Total Cost = {tot_cost:0.2f}')
+
+#f = open('Data/portfolio.csv','rt')
+def portfolio_cost(filename):
+    f = open(filename,'rt')
+    tot_cost = 0
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        shares = int(row[1])
+        share_price = float(row[2])
+        tot_cost += share_price * shares
+    return tot_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total Cost:', cost)
