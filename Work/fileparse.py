edc0a0438ce0321cb0790a2f70e3f0bac09da62c
DillonHanlon <dfh031@mun.ca>
2020-07-17 11:06:11 -0230

    Exercise 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,93 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+
+        # Read the file headers
+        
+        if has_headers:
+            headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
+
+        records = []
+        for row in rows:
+            if not row:    # Skip rows with no data
+                continue
+
+            if indices:
+                row = [ row[index] for index in indices]
+
+            if types:
+                row = [func(val) for func, val in zip(types, row) ]
+            if has_headers:
+               record = dict(zip(headers,row))
+            else: 
+                record = tuple(row)
+
+            records.append(record)
+
+    return records
+
+
+
+
+
+
+
+
+# def parse_csv(filename,select = None,types = None,has_headers = True):
+#     '''
+#     Parse a CSV file into a list of records
+#     '''
+#     with open(filename) as f:
+#         rows = csv.reader(f)
+
+#         # Read the file headers
+#         # headers = next(rows)
+#         if has_headers:
+#             headers = next(rows)
+
+#         if select:
+#             indices = [headers.index(colname) for colname in select]
+#             headers = select
+#         else:
+#             indices = []
+        
+        
+#         records = []
+#         for row in rows:
+#             if not row:    # Skip rows with no data
+#                 continue
+#             if indices:
+#                 row = [row[index] for index in indices]
+
+#             if types:
+#                 row = [func(val) for func, val in zip(types, row) ]  
+#             record = dict(zip(headers, row))
+
+#             if has_headers:
+#                 record = dict(zip(headers,row))
+#             else: 
+#                 record = tuple(row)
+#             records.append(record)
+
+#     return records
+
+# records = parse_csv('Data/portfolio.csv',types = [str, int, float])
+# print(records)
+# records = parse_csv('Data/portfolio.csv',types = [str, int], select = ['name','shares'])
+# print(records)
+# records = parse_csv('Data/prices.csv')
+# print(records)
