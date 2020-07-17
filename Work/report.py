1e703aee9562b47882fa54d719749798d2ce468d
DillonHanlon <dfh031@mun.ca>
2020-07-17 11:45:26 -0230

    Exercise 3.16

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,8 @@
 # report.py
+# Exercise 3.16
 import csv
 from fileparse import parse_csv
+
 
 def read_portfolio(filename):
     return parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
